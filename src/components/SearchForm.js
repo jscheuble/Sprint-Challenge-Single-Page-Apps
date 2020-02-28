febd67beefb4e2b564240e81acf422fcedb1f3a2
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-02-28 09:46:57 -0800

    completed mvp requirements

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+
 
 export default function SearchForm(props) {
  
   return (
-    <section className="search-form">
-      <form>
+    <section className='search-form'>
+      <form className='search'>
         <input type='text' name='search' value={props.query} placeholder='search &#x1F50E; by name' autoComplete='off' onChange={props.handleChange} />
       </form>
     </section>
